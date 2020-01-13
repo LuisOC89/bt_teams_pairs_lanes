b0d9b196a7bbd91060a974e9cacfb2d711bf5902
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2020-01-12 23:01:36 -0500

    Finished assign lanes to pairs of teams logic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import csv
 import sys
 import random
 import copy
+import string
 import pdb
 
 def main():
@@ -17,10 +18,19 @@ def main():
 
     total_pairs, max_teams_per_pair, initial_lane = insert_pairs_and_lanes(teams)
     
-    pairs_and_teams = handle_pairs_and_teams(teams, total_pairs, max_teams_per_pair, initial_lane)
+    pairs_and_teams = handle_pairs_and_teams(teams, total_pairs, max_teams_per_pair)
     
-    # teams_and_lanes = 
+#     TOTEST = {'Pair #1': [{'Team 83': ["David O'Mygod", 'Dalton Sorry']}, {'Team 99': ['Diego Coca', 'Kat MaxCubos']}, {'Team 86': ['Austin Texas', 'Austin Tailored']}, {'Team 03': ['Troy Amazon', 'Luis Cuban']}],
+#  'Pair #2': [{'Team 02': ['Whale Willy', 'Danika Patrykc']}, {'Team 01': ['Geico Buffet', 'Tiger Wuds']}, {'Team 82': ['Colin Loser', 'Hal Keys']}],
+#  'Pair #3': [{'Team 80': ['Teddy Crux', 'Ricardo Martinez']},
+#              {'Team 81': ['Jamie Bailes', 'Jorge Snyder']},
+#              {'Team 84': ['Tim Merlin', 'Marco Aurelious']},
+#              {'Team 85': ['Chip DeMexico', 'Khaleel Krypton']}],
+#  'Pair #4': [{'Team 87': ['Tirus Rex', 'Darion Byte']}, {'Team 88': ['Billy ElNino', 'Steve Brown']}, {'Team 100': ['Stephanie McFerrari', 'Rafa Peque�o']}]}
 
+    lanes_and_teams = handle_lanes_and_teams(pairs_and_teams, max_teams_per_pair, initial_lane)
+
+    # lanes_and_teams = handle_lanes_and_teams(TOTEST, 4, 5)
     input("\nPress Enter to continue...")
 
 def get_file_data():
@@ -100,16 +110,19 @@ def insert_pairs_and_lanes(teams):
                 teams_per_pair_int = int(max_teams_per_pair)
                 teams_per_pair_float = float(max_teams_per_pair)
                 if teams_per_pair_int != teams_per_pair_float or teams_per_pair_int <= 0:
-                    raise Exception
+                    raise Exception("The value of teams per pair has to be a whole positive number, please try again... ")
+                elif teams_per_pair_int > len(list(string.ascii_uppercase)):
+                    # ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' -> Max 26 teams per pair
+                    raise Exception("The value of teams per pair has to be a whole positive number, please try again... ")
                 teams_per_pair = teams_per_pair_int
                 break
-            except:
-                input("The value of teams per pair has to be a whole positive number, please try again... ")
+            except Exception as ex:
+                input(ex)
                 continue
         
         capacity = teams_per_pair * total_pairs
         if capacity < teams_quantity:
-            print(f"capacity = {total_pairs} pairs of lanes * {teams_per_pair} teams per pair = {capacity} spaces available.")
+            print(f"Capacity = {total_pairs} pairs of lanes * {teams_per_pair} teams per pair = {capacity} spaces available.")
             input(f"Number of teams ({teams_quantity} teams) exceeds the capacity " 
             f"({capacity} spaces available) of the place.\nPlease try again... ")
         else:
@@ -149,7 +162,7 @@ def see_specific_team(teams):
         else:
             pass
 
-def handle_pairs_and_teams(teams, total_pairs, max_teams_per_pair, initial_lane):
+def handle_pairs_and_teams(teams, total_pairs, max_teams_per_pair):
     # filled_pairs_of_lanes_list = ["Pair1", "Pair2", "Pair3"]
     # pairs_team_players_dict = {
     #     "Pair1":[
@@ -157,12 +170,6 @@ def handle_pairs_and_teams(teams, total_pairs, max_teams_per_pair, initial_lane)
     #         {"Team2":["Player1", "Player2"]}
     #     ]}
     
-    # filled_pairs_team_players_lanes_dict = {
-    #     "Pair1":[
-    #         {"Team1":{"Player1": "Lane 3A", "Player2": "Lane 4A"}},
-    #         {"Team2":{"Player1": "Lane 3B", "Player2": "Lane 4B"}}
-    #     ]}
-
     available_teams_list = list(teams.keys())
     pairs_team_players_dict = {}
     available_pairs_of_lanes = []
@@ -279,7 +286,6 @@ def handle_pairs_and_teams(teams, total_pairs, max_teams_per_pair, initial_lane)
                                 print("- Empty pair")
                             else:
                                 for team in list(pairs_team_players_dict[pair_number]):
-                                    # pdb.set_trace()
                                     print(f"-> {list(team.keys())[0]}: {list(team.values())[0]}")
                                     
                             print(f"Team to add:\n+ {random_team}: {teams[random_team]}")
@@ -291,8 +297,7 @@ def handle_pairs_and_teams(teams, total_pairs, max_teams_per_pair, initial_lane)
                             if len(pairs_team_players_dict[pair_number]) == max_teams_per_pair:
                                 current_available_pairs_of_lanes.remove(pair_number)
                                 filled_pairs_of_lanes.append(pair_number)
-                                # filled_pairs_of_lanes.append(available_pairs_of_lanes.pop(available_pairs_of_lanes.index(f"{pair_number}")))
-
+                                
                         if len(available_teams_list) == 0:
                             print(f"\navailable_teams_list: {available_teams_list}"
                             f"\npairs_team_players_dict: {pairs_team_players_dict}"
@@ -305,6 +310,57 @@ def handle_pairs_and_teams(teams, total_pairs, max_teams_per_pair, initial_lane)
             pass
     
     return pairs_team_players_dict
+
+def handle_lanes_and_teams(pairs_and_teams, max_teams_per_pair, initial_lane):
+    # From this:
+    # pairs_team_players_dict = {
+    #     "Pair1":[
+    #         {"Team1":["Player1", "Player2"]},
+    #         {"Team2":["Player1", "Player2"]}
+    #     ],
+    #     "Pair2":[
+    #         {"Team1":["Player1", "Player2"]},
+    #         {"Team2":["Player1", "Player2"]},
+    #         {"Team3":["Player1", "Player2"]}
+    #     ],
+    #     "Pair3":[
+    #         {"Team1":["Player1", "Player2"]}
+    #     ],
+    #     "Pair4":[]
+    # }
+
+    # To this:
+    # filled_pairs_team_players_lanes_dict = {
+    #     "Pair1":[
+    #         {"Team1":{"Player1": "Lane 3A", "Player2": "Lane 4A"}},
+    #         {"Team2":{"Player1": "Lane 3B", "Player2": "Lane 4B"}}
+    #     ]}
+
+    # ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' -> Max 26 teams per pair
+    new_dict = dict.fromkeys(pairs_and_teams.keys(),{})
+
+    possible_lane_letters = list(string.ascii_uppercase)
+    counter_odd = initial_lane
+    for k, children in pairs_and_teams.items():
+        new_children = []
+        letter_counter = 0
+        for child in children:
+            letter = possible_lane_letters[letter_counter]
+            tmp_dict = {}
+            tmp_dict[list(child.keys())[0]] = {}
+            for value in list(child.values())[0]:
+                if ((list(child.values())[0]).index(value) + 1) % 2 == 0:
+                    tmp_dict[list(child.keys())[0]][value] = f"Lane {counter_odd + 1}{letter}"
+                else:
+                    tmp_dict[list(child.keys())[0]][value] = f"Lane {counter_odd}{letter}"
+                
+            new_children.append(tmp_dict)
+            letter_counter += 1
+
+        new_dict[k] = new_children
+        counter_odd += 2
+
+    return new_dict
 
 if __name__ == "__main__":
     main()
--- tester.py
+++ tester.py
@@ -7,23 +7,23 @@ import string
 import pdb
 
 def main():
-    # pairs_team_players_dict = {
-    #     "Pair1":[
-    #         {"Team1":["Player1", "Player2"]},
-    #         {"Team2":["Player1", "Player2"]}
-    #     ],
-    #     "Pair2":[
-    #         {"Team1":["Player1", "Player2"]},
-    #         {"Team2":["Player1", "Player2"]},
-    #         {"Team3":["Player1", "Player2"]}
-    #     ],
-    #     "Pair3":[
-    #         {"Team1":["Player1", "Player2"]}
-    #     ]
-    # }
+    pairs_team_players_dict = {
+        "Pair1":[
+            {"Team1":["Player1", "Player2"]},
+            {"Team2":["Player1", "Player2"]}
+        ],
+        "Pair2":[
+            {"Team1":["Player1", "Player2"]},
+            {"Team2":["Player1", "Player2"]},
+            {"Team3":["Player1", "Player2"]}
+        ],
+        "Pair3":[
+            {"Team1":["Player1", "Player2"]}
+        ]
+    }
 
-    new_children = []
-    counter_odd = 3
+    
+    
     children = [
         {"Team1":["Player1", "Player2"]},
         {"Team2":["Player1", "Player2"]}
@@ -40,25 +40,30 @@ def main():
 
     # new_children.append(dict.fromkeys(child.keys(),{}))
 
+    new_dict = dict.fromkeys(pairs_team_players_dict.keys(),{})
+
     possible_lane_letters = list(string.ascii_uppercase)
-    
-    letter_counter = 0
-    for child in children:
-        letter = possible_lane_letters[letter_counter]
-        tmp_dict = {}
-        tmp_dict[list(child.keys())[0]] = {}
-        for value in list(child.values())[0]:
-            if ((list(child.values())[0]).index(value) + 1) % 2 == 0:
-                tmp_dict[list(child.keys())[0]][value] = f"Lane {counter_odd + 1}{letter}"
-            else:
-                tmp_dict[list(child.keys())[0]][value] = f"Lane {counter_odd}{letter}"
-            
-        new_children.append(tmp_dict)
-        letter_counter += 1
+    counter_odd = 3
+    for k, children in pairs_team_players_dict.items():
+        new_children = []
+        letter_counter = 0
+        for child in children:
+            letter = possible_lane_letters[letter_counter]
+            tmp_dict = {}
+            tmp_dict[list(child.keys())[0]] = {}
+            for value in list(child.values())[0]:
+                if ((list(child.values())[0]).index(value) + 1) % 2 == 0:
+                    tmp_dict[list(child.keys())[0]][value] = f"Lane {counter_odd + 1}{letter}"
+                else:
+                    tmp_dict[list(child.keys())[0]][value] = f"Lane {counter_odd}{letter}"
+                
+            new_children.append(tmp_dict)
+            letter_counter += 1
 
-    counter_odd += 2
+        new_dict[k] = new_children
+        counter_odd += 2
 
-    return new_children
+    return new_dict
 
 if __name__ == "__main__":
     main()
